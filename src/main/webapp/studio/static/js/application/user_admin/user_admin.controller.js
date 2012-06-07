cdf083a59fe4fd60a101a2546cb739f10e35fcab
Jason Madigan <jason.madigan@feedhenry.com>
2012-06-07 15:42:35 +0100

    Fixed layout

--- src/main/webapp/studio/static/js/application/user_admin/user_admin.controller.js
+++ src/main/webapp/studio/static/js/application/user_admin/user_admin.controller.js
@@ -21,7 +21,9 @@ UserAdmin.Controller = Class.extend({
     // TODO: Fix eval
     eval(this.models.user).list(function(res) {
       $('#useradmin_users_table').dataTable({
-        "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+        "bDestroy": true,
+        "bAutoWidth": false,
+        "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
         "sPaginationType": "bootstrap",
         "oLanguage": {
           "sLengthMenu": "_MENU_ records per page"
