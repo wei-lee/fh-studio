cfe3b0b528b87b149b822ca0ba3034dae9678407
Jason Madigan <jason.madigan@feedhenry.com>
2012-06-07 10:16:47 +0100

    Changed user groups for showing user admin

--- src/main/webapp/studio/static/js/application/accordion/UserAdminAccordionManager.js
+++ src/main/webapp/studio/static/js/application/accordion/UserAdminAccordionManager.js
@@ -7,36 +7,4 @@ application.UserAdminAccordionManager = application.AccordionManager.extend({
   postSelectUsersList: function() {
     debugger;
   }
-    
-  // postSelectAppsList: function () {
-  //   $fw_manager.client.arm.apps.initGrid();
-  // },
-  
-  // postSelectUsersList: function () {
-  //   $fw_manager.client.arm.users.initGrid();
-  // },
-  
-  // postSelectUsersAdd: function(){
-  //   $fw_manager.client.arm.users.initAdd();
-  // },
-
-  // postSelectGroupsList: function () {
-  //   $fw_manager.client.arm.groups.initGrid();
-  // },
-  
-  // postSelectGroupsAdd: function(){
-  //   $fw_manager.client.arm.groups.initAdd();
-  // },
-
-  // postSelectDevicesList: function () {
-  //   $fw_manager.client.arm.devices.initGrid();
-  // },
-  
-  // postSelectDevicesAdd: function(){
-  //   $fw_manager.client.arm.devices.initAdd();
-  // },
-  
-  // postSelectDevicesApprove: function(){
-  //   $fw_manager.client.arm.devices.initApproveGrid();
-  // }
 });
