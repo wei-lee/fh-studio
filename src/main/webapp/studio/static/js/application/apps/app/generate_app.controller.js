3bf1637b859f13e28c129a14e05334955ded416a
Jason Madigan <jason.madigan@feedhenry.com>
2012-06-13 10:11:52 +0100

    Changed path

--- src/main/webapp/studio/static/js/application/apps/app/generate_app.controller.js
+++ src/main/webapp/studio/static/js/application/apps/app/generate_app.controller.js
@@ -471,13 +471,13 @@ GenerateApp.Controllers.Wufoo = Class.extend({
 });
 
 GenerateApp.Controllers.WufooSingle = GenerateApp.Controllers.Wufoo.extend({
-  template_url: "https://bitbucket.org/jasonmadigan/wufootest/get/master.zip",
+  template_url: "https://github.com/feedhenry/Wufoo-Template/zipball/master",
   container: '#wufoo_single_generator_form',
   type: 'single'
 });
 
 GenerateApp.Controllers.WufooMulti = GenerateApp.Controllers.Wufoo.extend({
-  template_url: "https://bitbucket.org/jasonmadigan/wufootest/get/master.zip",
+  template_url: "https://github.com/feedhenry/Wufoo-Template/zipball/master",
   container: '#wufoo_multi_generator_form',
   type: 'multi'
 });
