1b0142af0e0f05e272fee62ae0da5ab002ea282a
Jason Madigan <jason.madigan@feedhenry.com>
2012-06-19 16:09:07 +0100

    Fixed some JSHint errors

--- src/main/webapp/studio/static/common/js/util/QueryStringParser.js
+++ src/main/webapp/studio/static/common/js/util/QueryStringParser.js
@@ -1,13 +1,12 @@
 QueryStringParser = {};
 
 QueryStringParser.getParameterByName = function(name) {
-  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
   var regexS = "[\\?&]" + name + "=([^&#]*)";
   var regex = new RegExp(regexS);
   var results = regex.exec(window.location.search);
   if (results == null) {
     return "";
   } else {
-    return decodeURIComponent(results[1].replace(/\+/g, " "))
-  };
-}
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+};
