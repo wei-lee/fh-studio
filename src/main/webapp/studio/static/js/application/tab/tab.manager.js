e5635331693aec8f85dbe56420d4ce2d08b42bc4
David Martin <david.martin@feedhenry.com>
2012-07-18 15:59:46 +0100

    re #1891 Readded breadcrumb support in new tab layouts

--- src/main/webapp/studio/static/js/application/tab/tab.manager.js
+++ src/main/webapp/studio/static/js/application/tab/tab.manager.js
@@ -1,35 +1,91 @@
 var Tab = Tab || {};
 Tab.Manager = Class.extend({
   controllers: {},
+  inited: false,
 
   init: function () {
-    // TODO: bind events to nav-list
-    var self = this;
-    var el = $('#' + this.id);
-    var navList = el.find('.nav-list');
-    navList.find('a').each(function (index, element) {
-      var jqEl = $(this);
-      var controllerName = jqEl.data('controller');
-      if ('undefined' === typeof self.controllers[controllerName]) {
-        var upperName = self.toUpper(controllerName);
-        try {
-          self.controllers[controllerName] = new (eval(upperName))(); // ok to use eval here
-        } catch (e) {
-          console.error('Make user ' + upperName + ' is loaded and defined');
-          throw e;
+
+  },
+
+  show: function () {
+    if (!this.inited) {
+      this.inited = true;
+
+      var self = this;
+      var el = $('#' + this.id);
+      var navList = el.find('.nav-list');
+      var crumb = el.find('.breadcrumb');
+
+      // bind events to each navlist items
+      navList.find('a').each(function (index, element) {
+        var jqEl = $(this);
+        var controllerName = jqEl.data('controller');
+
+        // resolve the right controller for the navlist item
+        if ('undefined' === typeof self.controllers[controllerName]) {
+          var upperName = self.toUpper(controllerName);
+          try {
+            self.controllers[controllerName] = new (eval(upperName))(); // ok to use eval here
+          } catch (e) {
+            console.error('Make user ' + upperName + ' is loaded and defined');
+            throw e;
+          }
         }
-      }
-      var controller = self.controllers[controllerName];
-      jqEl.on('click', function (e) {
-        controller.show();
+        var controller = self.controllers[controllerName];
+
+        // handler for when navlist item is clicked
+        jqEl.on('click', function (e) {
+          // update active status of navlist
+          navList.find('li').removeClass('active');
+          jqEl.closest('li').addClass('active');
+
+          // udpate breadcrumbs
+          var crumbs = [jqEl.text()];
+          var header = jqEl.closest('li').prev('.nav-header');
+          if (header.length > 0) {
+            crumbs.unshift(header.text());
+          }
+          self.updateCrumbs(crumbs);
+
+          // tell controller we're ready for it to do it's thing
+          controller.show();
+        });
       });
-    });
 
-    // TODO: bind breadcrumb updates
-    var crumb = el.find('.breadcrumb');
+      // meh, lets start with first item
+      // TODO: state stuff here
+      navList.find('a:eq(0)').trigger('click');
+    }
+  },
 
-    // TODO: initialise init state of tab
+  // see http://twitter.github.com/bootstrap/components.html#breadcrumbs
+  updateCrumbs: function (crumbs) {
+    var el = $('#' + this.id);
+    var crumb = el.find('.breadcrumb').empty();
 
+    for (var ci = 0, cl = crumbs.length; ci < cl; ci += 1) {
+      var ct = crumbs[ci];
+
+      if (ci !== (cl - 1)) {
+        // non-final crumb
+        crumb.append($('<li>').append($('<a>', {
+          "href": "#",
+          "text": ct
+        }).on('click', function (e) {
+          // TODO: implement
+          console.error('IMPLEMENT breadcrumb click');
+        })).append($('span', {
+          "class": "divider",
+          "text": "/"
+        })));
+      } else {
+        // final crumb
+        crumb.append($('<li>', {
+          "class": "active",
+          "text": ct
+        }));
+      }
+    }
   },
 
   toUpper: function (str) {
