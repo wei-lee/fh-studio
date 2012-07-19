4d18bdb82e92612646003e02811810459fec8208
Jason Madigan <jason.madigan@feedhenry.com>
2012-07-19 15:47:40 +0100

    StoreItem model

--- src/main/webapp/studio/static/js/application/admin/admin.storeitems.controller.js
+++ src/main/webapp/studio/static/js/application/admin/admin.storeitems.controller.js
@@ -3,7 +3,39 @@ var Admin = Admin || {};
 Admin.Storeitems = Admin.Storeitems || {};
 
 Admin.Storeitems.Controller = Controller.extend({
+  models: {
+    store_item: new model.StoreItem()
+  },
+
+  views: {
+    store_items: "#admin_store_items",
+    store_item_create: "#admin_store_item_create",
+    store_item_update: "#admin_store_item_update"
+  },
+
   init: function () {
-    // TODO: implement Storeitems stuff
+  },
+
+  show: function(e) {
+    console.log('show store items!');
+    this.showStoreItems();
+  },
+
+  hide: function(e) {
+    $.each(this.views, function(k, v) {
+      $(v).hide();
+    });
+  },
+
+  showStoreItems: function() {
+    var self = this;
+    this.hide();
+    $(this.views.store_items).show();
+    // this.models.group.list(function(res) {
+    //   self.renderGroupTable(res);
+    //   self.bindGroupControls();
+    // }, function(err) {
+    //   console.error(err);
+    // }, true);
   }
 });
---
+++ src/main/webapp/studio/static/js/model/models/StoreItem.js
@@ -0,0 +1,11 @@
+model.StoreItem = model.Model.extend({
+
+  // Model config
+  config: {},
+
+  init: function() {},
+
+  list: function(success, fail) {
+  }
+
+});
