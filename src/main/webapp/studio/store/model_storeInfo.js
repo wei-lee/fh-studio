b150c4ded2a0a4b2a776ca1826bc6e638cedd425
Martin Murphy <martin.murphy@feedhenry.com>
2012-07-30 15:17:04 +0100

    limit installs based on useragent

--- src/main/webapp/studio/store/model_storeInfo.js
+++ src/main/webapp/studio/store/model_storeInfo.js
@@ -10,5 +10,5 @@ model.StoreInfo = model.Model.extend({
     var MAM_STORE_INFO_URL = '/box/srv/1.1/mas/appstore/read';
     var params = {};
     return this.serverPost(MAM_STORE_INFO_URL, params, success, fail);
-  }
+  },
 });
