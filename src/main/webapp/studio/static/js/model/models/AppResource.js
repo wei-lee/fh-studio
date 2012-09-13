2c4be3019e745a7bd281ca05de38e1cfc2d2a7f6
David Martin <david.martin@feedhenry.com>
2012-09-13 15:58:30 +0100

    Added sample data for resources

--- src/main/webapp/studio/static/js/model/models/AppResource.js
+++ src/main/webapp/studio/static/js/model/models/AppResource.js
@@ -5,13 +5,30 @@ model.AppResource = model.Model.extend({
   },
 
   current: function (guid, env, success, fail) {
-    var url = Constants.APP_RESOURCES_URL;
-    var params = {
-      "guid": guid, // instance guid
-      "deploytarget": env
-    };
-
-    return this.serverPost(url, params, success, fail, true);
+    if ($fw.getClientProp('cloudresources-sampledata-enabled') === 'true') {
+      return success({
+        "status": "ok",
+        "data": {
+          "max":{
+            "cpu":100,
+            "disk":104857600,
+            "mem":268435456
+          },
+          "usage": {
+            "cpu":3.7,
+            "disk":26214400,
+            "mem":12582912
+          }
+        }
+      });
+    } else {
+      var url = Constants.APP_RESOURCES_URL;
+      var params = {
+        "guid": guid, // instance guid
+        "deploytarget": env
+      };
+      return this.serverPost(url, params, success, fail, true);
+    }
   },
 
   history: function (guid, env, success, fail) {
