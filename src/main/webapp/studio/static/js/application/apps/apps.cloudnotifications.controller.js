34167a7ba6530080d017c8d8a618a507f9e0def6
Jason Madigan <jason.madigan@feedhenry.com>
2012-09-18 12:31:24 +0100

    Sample notifications behind feature flag

--- src/main/webapp/studio/static/js/application/apps/apps.cloudnotifications.controller.js
+++ src/main/webapp/studio/static/js/application/apps/apps.cloudnotifications.controller.js
@@ -24,11 +24,59 @@ Apps.Cloudnotifications.Controller = Apps.Cloud.Controller.extend({
     this.initFn();
     this.showResources();
     $(this.container).show();
+
+    if ($fw.getClientProp("demo-ui-enabled") === "true") {
+      this.showSample();
+    }
+  },
+
+  showSample: function() {
+    function randomTS() {
+      return moment().subtract('minutes', Math.floor(Math.random() * 10 + 1)).format('MMMM Do YYYY, h:mm:ss a');
+    }
+
+    $('table.sample_ui', this.container).dataTable({
+      "bDestroy": true,
+      "bAutoWidth": false,
+      "sDom": "<'row-fluid'<'span12'>r>t<'row-fluid'<'span6'i><'span6'p>>",
+      "bPaginate": false,
+      "bLengthChange": false,
+      "bInfo": false,
+      "aaData": [
+        [randomTS(), '<span class="label label-warning">Restarted</span>', "App Cloud successfully restarted on FeedHenry"],
+        [randomTS(), '<span class="label label-important">Stopped</span>', "App Cloud stopped on FeedHenry"],
+        [randomTS(), '<span class="label label-success">Deployed</span>', "App Cloud successfully deployed to FeedHenry."],
+        [randomTS(), '<span class="label label-warning">Restarted</span>', "App Cloud successfully restarted on FeedHenry"],
+        [randomTS(), '<span class="label label-success">Deployed</span>', "App Cloud successfully deployed to FeedHenry."],
+        [randomTS(), '<span class="label label-warning">Restarted</span>', "App Cloud successfully restarted on FeedHenry"],
+        [randomTS(), '<span class="label label-important">Stopped</span>', "App Cloud stopped on FeedHenry"],
+        [randomTS(), '<span class="label label-success">Deployed</span>', "App Cloud successfully deployed to FeedHenry."],
+        [randomTS(), '<span class="label label-info">Stopped</span>', "App Cloud was halted after exhausting resources"],
+        [randomTS(), '<span class="label label-warning">Restarted</span>', "App Cloud successfully restarted on FeedHenry"],
+        [randomTS(), '<span class="label label-important">Stopped</span>', "App Cloud stopped on FeedHenry"],
+        [randomTS(), '<span class="label label-success">Deployed</span>', "App Cloud successfully deployed to FeedHenry."],
+        [randomTS(), '<span class="label label-info">Stopped</span>', "App Cloud was halted after exhausting resources"],
+        [randomTS(), '<span class="label label-warning">Restarted</span>', "App Cloud successfully restarted on FeedHenry"],
+        [randomTS(), '<span class="label label-important">Stopped</span>', "App Cloud stopped on FeedHenry"],
+        [randomTS(), '<span class="label label-success">Deployed</span>', "App Cloud successfully deployed to FeedHenry."],
+        [randomTS(), '<span class="label label-warning">Restarted</span>', "App Cloud successfully restarted on FeedHenry"],
+        [randomTS(), '<span class="label label-important">Stopped</span>', "App Cloud stopped on FeedHenry"]
+      ],
+      "aoColumns": [{
+        "sTitle": "Timestamp",
+        "sWidth": "200px"
+      }, {
+        "sTitle": "Notification Type",
+        "sWidth": "100px"
+      }, {
+        "sTitle": "Details"
+      }]
+    }).removeClass('hidden');
   },
 
   //TODO move this to apps.controller
   // type: error|success|info
-  showAlert: function (type, message) {
+  showAlert: function(type, message) {
     var self = this;
     var alerts_area = $(this.container).find('.alerts');
     var alert = $('<div>').addClass('alert fade in alert-' + type).html(message);
@@ -38,13 +86,13 @@ Apps.Cloudnotifications.Controller = Apps.Cloud.Controller.extend({
     // only automatically hide alert if it's not an error
     if ('error' !== type) {
       setTimeout(function() {
-        alert.slideUp(function () {
+        alert.slideUp(function() {
           alert.remove();
         });
       }, self.alert_timeout);
     }
   },
-  
+
   initBindings: function() {
     var self = this;
 
@@ -57,23 +105,20 @@ Apps.Cloudnotifications.Controller = Apps.Cloud.Controller.extend({
       var email = $('#notification_email', jqContainer).val();
       var guid = $fw.data.get('inst').guid;
       console.log("setting email id: " + guid + ", email: " + email);
-      self.models.notification_email.setEmail(guid, email, 
-        function(res){
-          console.log("success");
-          self.showAlert("success", "Successfully updated notification email address");
-        },
-        function(err){
-          console.log("failed to set email");
-          self.showAlert("error", "Error updating notification email address");
-        }
-      ); 
+      self.models.notification_email.setEmail(guid, email, function(res) {
+        console.log("success");
+        self.showAlert("success", "Successfully updated notification email address");
+      }, function(err) {
+        console.log("failed to set email");
+        self.showAlert("error", "Error updating notification email address");
+      });
     });
   },
 
   showResources: function(cb) {
     var self = this;
     var cfg = $fw.data.get('inst').config;
-    var email=cfg.notification_email;
+    var email = cfg.notification_email;
 
     $('#notification_email', $(this.container)).val(email);
   }
