df49f112f844c34249af8a2bf84c8752f6a45017
Martin Murphy <martin.murphy@feedhenry.com>
2012-09-20 02:04:02 +0100

    Fix resourse name to allo ios provisioning profile uploads - https://feedhenry.assembla.com/spaces/feedhenry-platform/tickets/2471

--- src/main/webapp/studio/static/js/model/ModelManager.js
+++ src/main/webapp/studio/static/js/model/ModelManager.js
@@ -4,6 +4,6 @@ model.ModelManager = Class.extend({
     this.App = new model.App();
     this.Template = new model.Template();
     this.User = new model.User();
-    this.Resource = new model.UserResource();
+    this.UserResource = new model.UserResource();
   }
 });
