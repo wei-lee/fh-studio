42683e059bf04841203b23f4cbb70a1c6192a525
Jason Madigan <jason.madigan@feedhenry.com>
2013-06-07 14:21:50 +0100

    Livequery added, links in charts

--- src/main/webapp/studio/static/js/views/analytics/app/app.analytics.dashboard.view.js
+++ src/main/webapp/studio/static/js/views/analytics/app/app.analytics.dashboard.view.js
@@ -1,9 +1,13 @@
 App.View.ProjectAppAnalyticsDashboard = Backbone.View.extend({
+  events: {
+    'click .highcharts-title tspan': 'show'
+  },
   initialize: function(options) {
     this.options = $.extend(true, {}, this.defaultOptions, options) || {};
   },
 
   render: function() {
+    var self = this;
     var html = $(this.template).html();
     var template = Handlebars.compile(html);
     this.$el.html(template());
@@ -21,12 +25,29 @@ App.View.ProjectAppAnalyticsDashboard = Backbone.View.extend({
       guid: this.options.guid,
       picker_model: this.options.picker_model
     }).$el);
-    
+
     this.$by_location.append(new this.views.byLocation({
       guid: this.options.guid,
       picker_model: this.options.picker_model
     }).$el);
 
+    // Title links
+    this.$el.find('.highcharts-title tspan').unbind().livequery('click', function(e) {
+      self.show(e);
+    });
+
     return this;
+  },
+
+  show: function(e) {
+    // TODO: This is a bit ghetto
+    try {
+      var title = $(e.currentTarget).text();
+      var match = title.split('by ')[1];
+      var nav_el = $('.nav.reporting_pills li a:contains(' + match + ')');
+      nav_el.trigger('click');
+    } catch (e) {
+      console.log("Couldn't find a nav item to trigger.");
+    }
   }
 });
