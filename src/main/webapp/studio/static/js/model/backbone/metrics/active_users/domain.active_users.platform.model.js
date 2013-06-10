6fe4697cf2a0edf1d377811f4d05fff5dce88555
Jason Madigan <jason.madigan@feedhenry.com>
2013-06-10 14:57:47 +0100

    More mocks for aggregated analytics

--- src/main/webapp/studio/static/js/model/backbone/metrics/active_users/domain.active_users.platform.model.js
+++ src/main/webapp/studio/static/js/model/backbone/metrics/active_users/domain.active_users.platform.model.js
@@ -1,5 +1,5 @@
 App.Collection.DomainActiveUsersPlatform = App.Collection.DomainPieMetrics.extend({
   model: App.Model.PieMetric,
-  url: "/studio/static/js/model/mocks/metrics/domain_transactions_dest.json",
+  url: "/studio/static/js/model/mocks/metrics/domain_active_users_dest.json",
   metric: "domaintransactionsdest"
 });
