48e4abd5e3143a72fa78fd2595b368500a87d083
cianclarke <cian@cianclarke.com>
2013-09-18 18:55:12 -0400

    More CMS piping

---
+++ src/main/webapp/studio/static/js/application/apps/apps.cms.controller.js
@@ -0,0 +1,19 @@
+var Apps = Apps || {};
+Apps.Cms = Apps.Cms || {};
+
+Apps.Cms.Controller = Apps.Cloud.Controller.extend({
+  container: '#cms_container',
+  init: function() {
+    this._super();
+  },
+  show: function(e) {
+    var self = this,
+    box_container = $($(this.container).find('.fh-box-inner'));
+    $(this.container).show();
+    if (this.view){
+      return;
+    }
+    this.view = new App.View.CMS();
+    box_container.empty().append(this.view.render().$el);
+  }
+});
--- src/main/webapp/studio/static/js/views/app/cloud/cms/cloud.cms.view.js
+++ src/main/webapp/studio/static/js/views/app/cloud/cms/cloud.cms.view.js
@@ -6,12 +6,28 @@ App.View.CMS = Backbone.View.extend({
   events: {
     'click table tbody tr': 'showDetails'
   },
-
+  templates : {
+    'cms_home' : '#cms_home'
+  },
   initialize: function(){
-
+    this.compileTemplates();
   },
   render: function(){
-    this.$el.append('hiworld');
+    this.$el.append(this.templates.$cms_home());
     return this;
-  }
+  },
+  compileTemplates: function() {
+    var templates = {};
+    for (var key in this.templates){
+      if (this.templates.hasOwnProperty(key)){
+        var tpl = this.templates[key],
+        html = $(tpl, this.$container).html();
+        if (!html){
+          throw new Error("No html found for " + key);
+        }
+        templates['$' + key] = Handlebars.compile(html);
+      }
+    }
+    this.templates = templates;
+  },
 });
