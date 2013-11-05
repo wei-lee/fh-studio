9ee51ba1f53debbab072b289475406aad134028e
cianclarke <cian@cianclarke.com>
2013-11-05 17:15:37 -0500

    More complete forms listing

--- src/main/webapp/studio/static/js/views/forms/forms.list.view.js
+++ src/main/webapp/studio/static/js/views/forms/forms.list.view.js
@@ -3,7 +3,7 @@ App.View = App.View || {};
 
 App.View.FormList = App.View.Forms.extend({
   templates : {
-    'formsListMenu' : '#formsListMenu'
+    'formsAddForm' : '#formsAddForm'
   },
   events : {
     'click .btn-add-form' : 'onCreateForm',
@@ -20,8 +20,6 @@ App.View.FormList = App.View.Forms.extend({
     this.$el.empty();
     this.$el.addClass('span10 formslist');
 
-    this.$el.append(this.templates.$formsListMenu());
-
     if (this.collection.length>0){
       this.renderList();
     }else{
@@ -37,6 +35,7 @@ App.View.FormList = App.View.Forms.extend({
     return this;
   },
   renderList : function(){
+    this.$el.append(this.templates.$formsAddForm());
     var self = this,
     //Needed fields eventually: Name, Version, Last Updated, Apps using, Submissions, Submissions Total
     columns = [{
@@ -64,7 +63,6 @@ App.View.FormList = App.View.Forms.extend({
       "bLengthChange": false,
       "iDisplayLength": 5,
       "bInfo": true,
-      "bFilter": false,
       "oLanguage" : {
         "sEmptyTable" : "No forms found"
       }
@@ -72,20 +70,28 @@ App.View.FormList = App.View.Forms.extend({
     this.table.render();
     this.table.$el.find('table').removeClass('table-striped');
     this.$el.append(this.table.$el);
+    
+    this.$el.append('<br />');
 
     // Add in the view form view
     this.selectMessage = new App.View.FullPageMessageView({ message : 'Select a form above to preview & manage', button : false });
     this.$el.append(this.selectMessage.render().$el);
 
-    this.$fbEl = $('<div></div>');
+    this.$fbEl = $('<div class="formpreview" />');
     this.$el.append(this.$fbEl);
     this.$fbEl.hide();
     this.fb = new Formbuilder(this.$fbEl, {
       noScroll : true,
       noEditOnDrop : true,
       bootstrapData: [{"hash" : "1a2b3c4d", "_id" : "123", "label":"Please enter your clearance number","field_type":"text","required":true,"field_options":{},"cid":"c6"}],
-      editStructure : true
+      editStructure : false
+      //TODO: editValues : false mode..
     });
+
+    // Place holders that get filled when the user clicks a form
+    this.$fbEl.prepend("<p>Form Description</p>");
+    this.$fbEl.prepend("<h4>Form Title</h4>");
+
     this.$fbEl.find('.middle').removeClass('span6');
     this.$fbEl.find('.right').hide();
 
@@ -104,6 +110,9 @@ App.View.FormList = App.View.Forms.extend({
     var index = el.data('index'),
     form = this.collection.at(index),
     fields = this.formToFormBuilderFields(form.toJSON());
+
+    this.$fbEl.find('h4').html(form.get('Name'));
+    this.$fbEl.find('p').html(form.get('Description'));
 
     this.selectMessage.$el.hide();
     this.$fbEl.show();
