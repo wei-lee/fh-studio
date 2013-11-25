72dfeb0ad44cb7e6d4e5a5f34e8e1a21b09436a6
cbrookes <craig.brookes@feedhenry.com>
2013-11-25 09:41:17 +0000

    page rule model

--- src/main/webapp/studio/static/js/model/backbone/forms/pagerule.model.js
+++ src/main/webapp/studio/static/js/model/backbone/forms/pagerule.model.js
@@ -11,3 +11,14 @@ App.Model.PageRule = Backbone.RelationalModel.extend({
     options.success(this);
   }
 });
+
+App.Collection.PageRules = Backbone.Collection.extend({
+  initialize: function() {},
+  model: App.Model.FieldRule,
+  //url: '/studio/static/js/model/backbone/mocks/forms/themes.json', //TODO:
+  urlUpdate: '/api/v2/forms/form/fieldRules',//TODO:
+  sync: function (method, model, options) {
+    console.log("sync called for model");
+    this[method].apply(this, arguments);
+  }
+});
