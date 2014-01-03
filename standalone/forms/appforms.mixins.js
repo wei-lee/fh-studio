0d03c1f3bb7d2f05206bf8c15872afbf4949efb6
cianclarke <cian@cianclarke.com>
2014-01-03 10:46:58 +0000

    Document steps for using appforms standalone

--- standalone/forms/appforms.mixins.js
+++ standalone/forms/appforms.mixins.js
@@ -1,8 +1,10 @@
-App.Collection.Form.prototype.url = 'json/forms.json';
-App.Collection.FormThemes.prototype.url = 'json/themes.json';
-App.Model.FormBase.prototype.fetch = function(options){
-  return options.success(this.collection.at(0));
+var $fw = $fw || {};
+$fw.getUserProp = $fw.getUserProp || function(prop){
+  if (prop === "roles"){
+    return ["devadmin", "sub", "formsadmin", "formsviewer", "dev", "portaladmin", "formseditor", "analytics"];
+  }
 };
+
 Backbone.View.prototype.compileTemplates = function() {
   if (!this.templates){
     throw new Error('Error: compileTemplates called on view with no templates defined');
@@ -23,3 +25,21 @@ Backbone.View.prototype.compileTemplates = function() {
   }
   //this.templates = templates;
 };
+
+
+// This block for MOCK data
+App.Collection.Form.prototype.url = 'json/forms.json';
+App.Collection.FormThemes.prototype.url = 'json/themes.json';
+App.Model.FormBase.prototype.fetch = function(options){
+  return options.success(this.collection.at(0));
+};
+// End block for MOCK data
+
+
+// This block for LIVE data
+//$.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+//  options.url = options.url.replace(/\b.*?api\/v2/, 'https://testing.feedhenry.me/api/v2')
+//  options.headers = options.headers || {};
+//  options.headers['x-fh-auth-user'] = '5e37dd4f6fa26b42c1d8f455d602542bb8853c7e' // User api key header. Will require a preflight request from the browser with a Access-Control-Request-Headers header set.
+//});
+// End block for LIVE data
