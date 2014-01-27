79466f845f9f55dbeed2ddbd02a6b4e9d26159aa
cbrookes <craig.brookes@feedhenry.com>
2014-01-27 15:57:21 +0000

    remove ajax call from field rules

--- src/main/webapp/studio/static/js/model/backbone/forms/fieldrules.model.js
+++ src/main/webapp/studio/static/js/model/backbone/forms/fieldrules.model.js
@@ -12,79 +12,12 @@ App.Model.FieldRule = Backbone.RelationalModel.extend({
   }
 });
 
-App.Collection.FieldRules = Backbone.Collection.extend({
+App.Collection.FieldRules = App.Collection.FormBase.extend({
   initialize: function() {},
   model: App.Model.FieldRule,
   urlUpdate: '/api/v2/forms/form/fieldRules',
   sync: function (method, model, options) {
-    console.log("sync called for model");
+    model.formId = model.formid;
     this[method].apply(this, arguments);
-  },
-  read : function(method, model, options){
-    var self = this;
-
-    if(!self.loaded){
-      var url = self.url;
-      $.ajax({
-        type: 'GET',
-        url: url,
-        success: function(res){
-          if (res && res.themes && res.themes.length && res.themes.length>0) {
-            self.loaded = true;
-            if ($.isFunction(options.success)) {
-              options.success(res.themes, options);
-            }
-          } else {
-            if ($.isFunction(options.error)) {
-              options.error(res, options);
-            }
-          }
-        },
-        error: function(xhr, status){
-          options.error(arguments);
-        }
-      });
-    } else {
-      self.trigger("sync");
-    }
-  },
-  update : function(method, model, options){
-    var self = this;
-    var url = self.urlUpdate;
-
-
-
-
-    var data = {
-     "formId":model.formid,
-     "rules":model.rules
-    };
-
-    console.log("model ",data);
-
-    $.ajax({
-      type: 'POST',
-      url: url,
-      data: JSON.stringify(data),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function(res){
-        if (res) {
-          self.trigger('reset');
-          if ($.isFunction(options.success)) {
-            options.success(res, options);
-          }
-        } else {
-          if ($.isFunction(options.error)) {
-            options.error(res, options);
-          }
-        }
-      },
-      error: function(xhr, status){
-        if ($.isFunction(options.error)) {
-          options.error(arguments);
-        }
-      }
-    });
   }
 });
