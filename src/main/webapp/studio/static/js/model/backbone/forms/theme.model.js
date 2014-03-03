a1893a388b77af198245117ca4f5b053816d5796
cianclarke <cian@cianclarke.com>
2014-03-03 10:59:57 -0500

    Merge two theme views so same view class shared between nGUI & fh-studio

--- src/main/webapp/studio/static/js/model/backbone/forms/theme.model.js
+++ src/main/webapp/studio/static/js/model/backbone/forms/theme.model.js
@@ -7,7 +7,15 @@ App.collections = App.collections || {};
 
 App.Model.FormTheme = App.Model.FormBase.extend({
   idAttribute: '_id',
-  fetchURL : '/api/v2/forms/theme/{{id}}'
+  fetchURL : '/api/v2/forms/theme/{{id}}',
+  urlUpdate: '/api/v2/forms/theme',
+  save : function(attributes, options){
+    var self = this,
+    id = this.get('_id');
+    options.type = 'post'; // Always use POST for these operations
+    this.url = this.urlUpdate.replace('{{id}}', id);
+    Backbone.RelationalModel.prototype.save.apply(this, arguments);
+  }
 });
 
 App.Model.FormThemeTemplate = App.Model.FormBase.extend({
@@ -20,7 +28,6 @@ App.Collection.FormThemes = App.Collection.FormBase.extend({
   pluralName : 'themes',
   model: App.Model.FormTheme,
   url: '/api/v2/forms/theme',
-  urlUpdate: '/api/v2/forms/theme',
   create : function(method, model, options){
     // Add in the default theme spec to this..
     var name = model.name;
