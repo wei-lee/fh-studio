67c9eaee1d9aaa8ed77c8ec30270e8ab53a6c575
cbrookes <craig.brookes@feedhenry.com>
2014-04-02 09:52:09 +0000

    update formbuilder to be same as ngui update viewhelper to be same as ngui

--- src/main/webapp/studio/static/js/views/forms/viewhelpers/viewhelpers.js
+++ src/main/webapp/studio/static/js/views/forms/viewhelpers/viewhelpers.js
@@ -187,15 +187,15 @@ Handlebars.registerHelper("createFormField", function (options, editMode, contex
         break;
       case "location":
       case 'locationMap':
-        if(options.fieldOptions && options.fieldOptions.definition && "northEast" === options.fieldOptions.definition.locationUnit){
+        if(options.fieldOptions && options.fieldOptions.definition && "eastnorth" === options.fieldOptions.definition.locationUnit){
           if (editMode){
-            template = "Eastings: <input class='formVal' data-_id='{{_id}}'  name='eastings' {{disabled}} type='text' placeholder='No value present' value='{{eastings}}' data-index={{idx}} /><br />" +
-            "Northings: <input class='formVal' data-_id='{{_id}}'  name='northings' {{disabled}} type='text' placeholder='No value present' value='{{northings}}' data-index={{idx}} /><br />" +
-            "Zone: <input name='zone' class='formVal' data-_id='{{_id}}'  {{disabled}} type='text' placeholder='No value present' value='{{zone}}' data-index={{idx}} /><br />";
+            template = "Eastings: <input class='formVal' data-_id='{{_id}}'  name='eastings' {{disabled}} type='text' placeholder='No value present' value='{{val.eastings}}' data-index={{idx}} /><br />" +
+            "Northings: <input class='formVal' data-_id='{{_id}}'  name='northings' {{disabled}} type='text' placeholder='No value present' value='{{val.northings}}' data-index={{idx}} /><br />" +
+            "Zone: <input name='zone' class='formVal' data-_id='{{_id}}'  {{disabled}} type='text' placeholder='No value present' value='{{val.zone}}' data-index={{idx}} /><br />";
           }else{
-            template = "<label>Eastings:</label> {{eastings}}, <br />" +
-            "<label>Northings:</label> {{northings}}, <br />" +
-            "<label>Zone:</label> {{zone}}";
+            template = "<label>Eastings:</label> {{val.eastings}}, <br />" +
+            "<label>Northings:</label> {{val.northings}}, <br />" +
+            "<label>Zone:</label> {{val.zone}}";
           }
         }else{
           // Map link for non-northings eastings for convenience
